363d55d960e51104c8c776d40d0d640800782d70
gaston <gastonbohm@hotmail.com>
2023-12-20 12:09:32 -0300

    ultimos arreglos

--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 
+
 const ItemCount = ({ initial, stock, onAdd }) => {
 	const [count, setCount] = useState(parseInt(initial));
 	const decrease = () => {
