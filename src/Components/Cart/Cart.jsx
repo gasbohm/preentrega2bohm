c917014ddc50778615a286223cc8002075a495d1
gaston <gastonbohm@hotmail.com>
2024-01-10 01:17:33 -0300

    cambios formulario

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../Context/CartContext';
 import ItemCart from '../ItemCart/ItemCart';
+import CheckOut from '../Checkout/CheckOut';  // Importa el componente CheckOut
 
 const Cart = () => {
   const { cart, totalPrice } = useCartContext();
@@ -24,10 +25,11 @@ const Cart = () => {
       <Link to="/checkout">
         <button className="btn-total">Finalizar Compra</button>
       </Link>
+      {/* Agrega el componente CheckOut aquí */}
+      <CheckOut />
     </>
   );
 };
 
 export default Cart;
-
 
